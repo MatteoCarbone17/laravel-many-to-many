376afc375d38c177bf37014829771652f6a606c4
MatteoCarbone17 <matteocarbone17@live.com>
2023-03-01 15:30:47 +0100

    add model technology , belongtomany project-technology

--- app/Models/project.php
+++ app/Models/project.php
@@ -21,4 +21,8 @@ class project extends Model
     public function type(){
       return $this->belongsTo(Type::class);
     }
+
+    public function technologies(){
+      return $this->belongsToMany(Technology::class);
+  }
 }
